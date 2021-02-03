80e190074ab5f5a04afc8b04fec39f0c8d287901
狐日泽 <Sun_Set1102@outlook.com>
2021-02-04 04:53:39 +0800

    1

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -35,10 +35,10 @@ let fruitSlowDown = false;
 let clickChangeFruit = false;
 
 // 广告链接：false 或为空字符串表示不会跳转到广告
-let adLink = 'https://live.bilibili.com/7194086';
+let adLink = 'https://live.bilibili.com/22454405';
 
 // 修改网页标题：将 "合成大西瓜" 进行替换
-document.getElementsByTagName("title")[0].innerText = '合成大菜花';
+document.getElementsByTagName("title")[0].innerText = '合成大耶比';
 
 // 开启选分弹窗：将 false 改为 true
 let selectModal = false;
